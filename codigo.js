30b210938ea550f208ad35967be27aebe218825b
Marcos Aybar <aybarmarcos18@gmail.com>
2023-08-05 00:40:50 -0300

    completo el registro del usuario y terminando con el estilo

--- codigo.js
+++ codigo.js
@@ -1,5 +1,44 @@
-const btn = document.getElementById('boton');
+const botonColorMode = document.querySelector("#colorModo");
+const body = document.body;
 
- btn.addEventListener('click', () =>{
-    console.log('click');
- });
+let darkMode = localStorage.getItem("dark-mode");
+
+function activarDarkMode() {
+    body.classList.add("dark-mode");
+    localStorage.setItem("dark-mode", "activado");
+}
+
+function desactivarDarkMode() {
+    body.classList.remove("dark-mode");
+    localStorage.setItem("dark-mode", "desactivado");
+}
+
+botonColorMode.addEventListener("click", () => {
+    darkMode = localStorage.getItem("dark-mode");
+
+    if (darkMode === "activado") {
+        desactivarDarkMode();
+    } else {
+        activarDarkMode();
+    }
+})
+
+
+const form = document.getElementById('registro');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  localStorage.setItem('nombre', e.target[0].value);
+  localStorage.setItem('apellido', e.target[1].value);
+  localStorage.setItem('mail', e.target[2].value);
+
+  const nombre = document.querySelector("#nombre").value;
+  const apellido = document.querySelector("#apellido").value;
+
+  const el = document.getElementById('respuesta');
+  let parrafo = document.createElement('h3');
+  parrafo.innerText = `¡Bienvenido ${nombre} ${apellido} a nuestra tienda!`;
+  el.append(parrafo);
+
+});
+
